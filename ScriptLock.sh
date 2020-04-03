8a74d8e506a6506cd08bc4ac5736d18a03319fe1
mcaiazzo13 <42116669+mcaiazzo13@users.noreply.github.com>
2020-04-03 18:46:26 +0200

    Update ScriptLock.sh

--- ScriptLock.sh
+++ ScriptLock.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #take screenshot of actual desktop
-command scrot 'wally.png' -e 'mv $f /root/Downloads'
+command scrot 'wally.png' -e 'mv $f /Users/marco/Downloads'
 #modify the image 
-command convert -scale 10% -scale 1000% /root/Downloads/wally.png /root/Scaricati/wallyNew.png
+command convert -scale 10% -scale 1000% /Users/marco/Downloads/wally.png /Users/marco/Scaricati/wallyNew.png
 #call i3lock using my new pixelate image 
-command i3lock -i /root/Scaricati/wallyNew.png -f -d
+command i3lock -i /Users/marco/Downloads/wallyNew.png -f -d
 #delete original screenshot and the pixelate image
-command rm /root/Downloads/wally.png /root/Scaricati/wallyNew.png
+command rm /Users/marco/Downloads/wally.png /Users/marco/Downloads/wallyNew.png
