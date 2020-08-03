0f7175c4aac049380d048b0ca3ed6219038d7015
mcaiazzo13 <42116669+mcaiazzo13@users.noreply.github.com>
2020-08-03 15:35:22 +0200

    Update Lock_Script.sh

--- Lock_Script.sh
+++ Lock_Script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #take screenshot of actual desktop
 #screenshot is stored in the actual directory
-command scrot 'wally.png'
+command scrot /tmp/wally.png
 #modify the image 
-command convert -scale 10% -scale 1000% wally.png wallyNew.png
+command convert -scale 10% -scale 1000% /tmp/wally.png /tmp//wallyNew.png
 #call i3lock using my new pixelate image 
-command i3lock -i wallyNew.png -f -d
-#delete original screenshot and the pixelate image
-command rm wally.png wallyNew.png
+command i3lock -i /tmp/wallyNew.png -f -d
