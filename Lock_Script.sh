3c915761519a8897e06876ba4a989a502a45c99b
mcaiazzo13 <42116669+mcaiazzo13@users.noreply.github.com>
2020-09-30 15:50:28 +0200

    Update Lock_Script.sh

--- Lock_Script.sh
+++ Lock_Script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #take screenshot of actual desktop
-#screenshot is stored in the actual directory
+#screenshot is stored in the working directory
 command scrot /tmp/wally.png
 #modify the image 
 command convert -scale 10% -scale 1000% /tmp/wally.png /tmp//wallyNew.png
-#call i3lock using my new pixelate image 
+#execute i3lock using my new pixelate image 
 command i3lock -i /tmp/wallyNew.png -f -d
